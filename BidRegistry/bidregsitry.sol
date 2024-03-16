b291796331473381c3b598e7c569aa7138ba5834
Konrad <konrad.strachan@gmail.com>
2024-03-16 21:52:24 +0000

    Untested but theoretically now correctly donates funds to the pool

--- BidRegistry/bidregsitry.sol
+++ BidRegistry/bidregsitry.sol
@@ -2,7 +2,14 @@
 pragma solidity ^0.8.24;
 
 //import {PoolKey} from "v4-core/src/types/PoolKey.sol";
+// See https://github.com/Uniswap/v4-core/blob/main/src/types/PoolId.sol
 type PoolId is bytes32;
+
+// Definition from https://github.com/Uniswap/v4-core/blob/main/src/types/BalanceDelta.sol
+type BalanceDelta is int256;
+
+// Definition from https://github.com/Uniswap/v4-core/blob/main/src/types/Currency.sol
+type Currency is address;
 
 // TODO: SafeERC20
 interface IERC20 {
@@ -10,9 +17,29 @@ interface IERC20 {
     function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
 }
 
+// Definition from https://github.com/Uniswap/v4-core/blob/main/src/types/PoolKey.sol
+struct PoolKey {
+    Currency currency0;
+    Currency currency1;
+    uint24 fee;
+    int24 tickSpacing;
+    address hooks;
+}
+
+// From https://github.com/Uniswap/v4-core/blob/main/src/PoolManager.sol
+interface IUniswapV4 {
+    function donate(PoolKey memory key, uint256 amount0, uint256 amount1, bytes calldata hookData)
+        external
+        //override
+        //noDelegateCall
+        //isLocked
+        returns (BalanceDelta delta);
+}
+
 contract EscrowRegistry {
 
     address auctionMaster;
+    address hookAddress;
 
     struct LockedBalance {
         // Total amount held in contract
@@ -58,8 +85,9 @@ contract EscrowRegistry {
     mapping(address => 
         mapping(PoolId => Tokens)) public poolTokens;
 
-    constructor(address masterKey) {
+    constructor(address masterKey, address hooks) {
         auctionMaster = masterKey;
+        hookAddress = hooks;
     }
 
     function updateAuctionMasterKey(address updatedKey) public {
@@ -67,23 +95,27 @@ contract EscrowRegistry {
         auctionMaster = updatedKey;
     }
 
+    function updateHooks(address updatedHook) public {
+        // Relax, it's a hackathon
+        hookAddress = updatedHook;
+    }
+
     function registerNewPool(address v4Contract, PoolId id, address poolTokenA, address poolTokenB) public {
-        // TODO call pool get tokens
         poolTokens[v4Contract][id] = Tokens(poolTokenA, poolTokenB);
     }
 
-    function depositAndClaimOrdering(address v4Contract, PoolId id, address user, address token, uint256 amount, uint256 blockNumber) public {
+    function depositAndClaimOrdering(address v4Contract, PoolId id, address user, address token, uint256 amount, uint256 blockNumber, bytes32 bid, bytes memory sig) public {
         require(depositFunds(v4Contract, id, user, token, amount), "Failed to deposit");
-        require(claimPriorityOrdering(v4Contract, id, user, token, amount, blockNumber), "Failed to claim ordering");
+        require(claimPriorityOrdering(v4Contract, id, user, token, amount, blockNumber, bid, sig), "Failed to claim ordering");
     }
 
-    function claimPriorityOrdering(address v4Contract, PoolId id, address user, address token, uint256 amount, uint256 blockNumber, Bid memory bid, bytes memory sig) public returns (bool) {
-        require(verifySignature(bid, sig) == auctionMaster, "Auction master address mismatch");
+    function claimPriorityOrdering(address v4Contract, PoolId id, address user, address token, uint256 amount, uint256 blockNumber, bytes32 bid, bytes memory sig) public returns (bool) {
+        require(recoverSigner(bid, sig) == auctionMaster, "Auction master address mismatch");
         require(hasSufficientFundsToPayforOrdering(v4Contract, id, user, token, amount), "Insufficient funds");
-        
+
         // charge token
         escrow[v4Contract][id][token][user].amountSpent += amount;
-        
+
         // If ok, then set ordering priority
         priorityOrdering[v4Contract][id][user][blockNumber] = true;
         return true;
@@ -115,21 +147,49 @@ contract EscrowRegistry {
         return true;
     }
 
-    function enrichLPers() public {
-        // TODO work out how to donate to a pool
-        // TODO reset escrow[v4Contract][id][token][user].amount and spentAmounts
+    function enrichLPers(address v4Contract, PoolId id, address token, address user) public {
+        require(escrow[v4Contract][id][token][user].amount >= escrow[v4Contract][id][token][user].amountSpent, "Amounts mismatch");
+
+        address t0 = poolTokens[v4Contract][id].tokenA;
+        address t1 = poolTokens[v4Contract][id].tokenB;
+        PoolKey memory key = createPoolKey(t0, t1);
+        
+        uint256 amountToEnrich = escrow[v4Contract][id][token][user].amountSpent;
+        IUniswapV4(v4Contract).donate(key, amountToEnrich, 0, new bytes(0));
+
+        escrow[v4Contract][id][token][user].amount -= escrow[v4Contract][id][token][user].amountSpent;
+        escrow[v4Contract][id][token][user].amountSpent = 0;
     }
 
-    function verifySignature(Bid memory bid, bytes memory sig) public pure returns (address) {
-        bytes32 structHash = keccak256(abi.encode(bid));
-        bytes32 messageHash = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", structHash));
-        address signer = ecrecover(messageHash, 0, sig, 0);
-        require(signer != address(0), "Invalid signature");
-        return signer;
+    function recoverSigner(bytes32 _ethSignedMessageHash, bytes memory _signature) public pure returns (address) {
+        (bytes32 r, bytes32 s, uint8 v) = splitSignature(_signature);
+        return ecrecover(_ethSignedMessageHash, v, r, s);
     }
 
-    function checkValidtoken(address v4Contract, PoolId id, address token) public pure returns (bool) {
+    function splitSignature(bytes memory sig) public pure returns (bytes32 r, bytes32 s, uint8 v) {
+        require(sig.length == 65, "invalid signature length");
+        assembly {
+            r := mload(add(sig, 32))
+            s := mload(add(sig, 64))
+            v := byte(0, mload(add(sig, 96)))
+        }
+        if (v < 27) {
+            v += 27;
+        }
+    }
+
+    function checkValidtoken(address v4Contract, PoolId id, address token) public view returns (bool) {
         return poolTokens[v4Contract][id].tokenA == token;
+    }
+
+    // Borrowed from https://github.com/Uniswap/v4-periphery/blob/main/test/Quoter.t.sol#L535
+    function createPoolKey(address tokenA, address tokenB)
+        internal
+        view 
+        returns (PoolKey memory)
+    {
+        if (address(tokenA) > address(tokenB)) (tokenA, tokenB) = (tokenB, tokenA);
+        return PoolKey(Currency.wrap(address(tokenA)), Currency.wrap(address(tokenB)), 3000, 60, hookAddress);
     }
 
 }
